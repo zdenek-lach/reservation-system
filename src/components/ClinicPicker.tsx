483709a935178c4433d43ec928cacd88f287e6ff
Sidonius <lach.zdenek@gmail.com>
2023-11-11 22:53:53 +0100

    Refactored DoctorPicker and ClinicPicker, added loading spinners

--- src/components/ClinicPicker.tsx
+++ src/components/ClinicPicker.tsx
@@ -1,57 +1,45 @@
-// import { Box, Button, Grid, Image, Text } from '@chakra-ui/react';
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { setDoctorAndClinic } from '../actions/actions';
-
-// import Clinic from '../types/ClinicType';
+import { Center, Flex, Spinner, Text } from '@chakra-ui/react';
+import React from 'react';
+import { useAppContext } from '../context/AppContext';
+import { useClinics } from '../hooks/useClinics'; // Assuming you have a similar hook for clinics
+import ClinicCard from './ClinicCard';
 
 const ClinicPicker: React.FC = () => {
-  //   const navigate = useNavigate();
+  const { clinicList, selectedDoctor } = useAppContext();
+  const { loading, error } = useClinics(); // Assuming you have a similar hook for clinics
 
-  //   //todo load clinics from api
+  const getClinicsForSelectedDoctor = () => {
+    if (selectedDoctor != null && clinicList != null) {
+      return clinicList.filter((clinic) =>
+        selectedDoctor.clinics.includes(clinic.id)
+      );
+    }
+    return [];
+  };
 
-  //   const handleButtonClick = (selectedClinic: Clinic) => {
-  //     if (selectedDoctor !== null) {
-  //       setDoctorAndClinic(selectedDoctor, selectedClinic);
-  //       navigate('/calendar');
-  //     } else {
-  //       navigate('/error');
-  //     }
-  //   };
+  const renderClinicCarousel = () => {
+    if (!clinicList) {
+      return null;
+    }
+
+    return getClinicsForSelectedDoctor()?.map((clinic) => (
+      <ClinicCard key={clinic.id} clinic={clinic} />
+    ));
+  };
 
   return (
-    //     <Box maxW="container.xl" mx="auto" p={5}>
-    //       <Text fontSize="3xl" color="teal.500" mb={3}>
-    //         Zvolte si kliniku
-    //       </Text>
-    //       <Grid templateColumns={{ sm: 'repeat(2, 1fr)' }} gap={6}>
-    //         {clinics.map((clinic) => (
-    //           <Box key={clinic.id}>
-    //             <Image src={clinic.imageUrl} alt={clinic.name} w="100%" />
-    //             <Text fontSize="lg" color="teal.500" mt={1}>
-    //               {clinic.name}
-    //             </Text>
-    //             <Button
-    //               colorScheme="teal"
-    //               onClick={() => handleButtonClick(clinic)}
-    //             >
-    //               Zvolit {clinic.name}
-    //             </Button>
-    //           </Box>
-    //         ))}
-    //       </Grid>
-    //     </Box>
-    <></>
+    <Center py={6}>
+      <Flex direction="row" overflowX="scroll" py={5}>
+        {loading ? (
+          <Spinner />
+        ) : error ? (
+          <Text>{error}</Text>
+        ) : (
+          renderClinicCarousel()
+        )}
+      </Flex>
+    </Center>
   );
 };
 
-// const mapStateToProps = (state: any) => ({
-//   selectedDoctor: state.selectedDoctor,
-// });
-
-// const mapDispatchToProps = {
-//   setDoctorAndClinic,
-// };
-
-export default /*connect(mapStateToProps, mapDispatchToProps)(*/ ClinicPicker /*)*/;
+export default ClinicPicker;

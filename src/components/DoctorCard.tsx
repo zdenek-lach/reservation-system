9273095260ec3d159b350acc0496588d1465edeb
Sidonius <lach.zdenek@gmail.com>
2023-11-12 01:49:42 +0100

    Unfinished yet improved doctor card formatting.

--- src/components/DoctorCard.tsx
+++ src/components/DoctorCard.tsx
@@ -1,6 +1,9 @@
 import {
-  Box,
   Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
   Flex,
   Image,
   Spinner,
@@ -23,56 +26,53 @@ const DoctorCard: React.FC<{ doctor: Doctor }> = ({ doctor }) => {
   };
 
   return (
-    <Box
-      key={doctor.id}
-      mx={2}
-      maxW={'270px'}
-      w={'full'}
-      bg={'white'}
-      boxShadow={'2xl'}
-      rounded={'md'}
-      overflow={'hidden'}
-    >
-      <Flex justify={'center'} mt={12}>
-        {!imageLoaded && <Spinner />}
-        <Image
-          src={`/assets/doctorPics/${doctor.pictureId}.jpg`}
-          alt={`Doctor ${doctor.pictureId}`}
-          w="100%"
-          onLoad={() => setImageLoaded(true)}
-          style={imageLoaded ? {} : { display: 'none' }}
-        />
-      </Flex>
-
-      <Box p={6}>
-        <Stack spacing={0} align={'center'} mb={5}>
-          <Text
-            fontSize={'2xl'}
-            fontWeight={500}
-            fontFamily={'body'}
-            color={'gray.800'}
+    <>
+      <Card maxW="25%">
+        <CardHeader>
+          <Flex justify={'center'} mt={6}>
+            {!imageLoaded && <Spinner />}
+            <Image
+              src={`/assets/doctorPics/${doctor.pictureId}.jpg`}
+              alt={`Doctor ${doctor.firstName + doctor.lastName}`}
+              w="100%"
+              borderRadius="lg"
+              onLoad={() => setImageLoaded(true)}
+              style={imageLoaded ? {} : { display: 'none' }}
+            />
+          </Flex>
+        </CardHeader>
+        <CardBody>
+          <Stack spacing={0} align={'center'} mb={5}>
+            <Text
+              fontSize={'2xl'}
+              fontWeight={500}
+              fontFamily={'body'}
+              color={'gray.800'}
+            >
+              {doctor.firstName} {doctor.lastName}
+            </Text>
+            <Text color={'gray.500'}>{doctor.title}</Text>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <Button
+            w={'full'}
+            mt={8}
+            bg={'blue.500'}
+            color={'white'}
+            rounded={'md'}
+            _hover={{
+              bg: 'blue.600',
+              transform: 'translateY(-2px)',
+              boxShadow: 'lg',
+            }}
+            onClick={handleButtonClick}
           >
-            {doctor.firstName} {doctor.lastName}
-          </Text>
-          <Text color={'gray.500'}>{doctor.title}</Text>
-        </Stack>
-        <Button
-          w={'full'}
-          mt={8}
-          bg={'blue.500'}
-          color={'white'}
-          rounded={'md'}
-          _hover={{
-            bg: 'blue.600',
-            transform: 'translateY(-2px)',
-            boxShadow: 'lg',
-          }}
-          onClick={handleButtonClick}
-        >
-          Zvolit {doctor.firstName}
-        </Button>
-      </Box>
-    </Box>
+            Zvolit {doctor.firstName}
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
   );
 };
 

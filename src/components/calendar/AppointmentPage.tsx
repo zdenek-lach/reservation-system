1ddb359eb72fd2a13b1bddcecbdbb68fbc6c2a10
Sidonius <lach.zdenek@gmail.com>
2023-11-16 01:10:35 +0100

    First Calendar implementation + some edits and changes across  the board

---
+++ src/components/calendar/AppointmentPage.tsx
@@ -0,0 +1,123 @@
+import { Box, Flex, Heading, Table, Tbody, Td, Tr } from '@chakra-ui/react';
+import {
+  CalendarPanel,
+  Month_Names_Short,
+  Weekday_Names_Short,
+} from 'chakra-dayzed-datepicker';
+import {
+  addDays,
+  eachHourOfInterval,
+  format,
+  startOfWeek,
+  subDays,
+} from 'date-fns';
+import { useState } from 'react';
+import { useAppContext } from '../../context/AppContext';
+import AppointmentDoctorCard from './AppointmentDoctorCard';
+import Time from './Time';
+
+const AppointmentPage = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { selectedDoctor } = useAppContext();
+  const weekStart = startOfWeek(selectedDate);
+  const weekEnd = addDays(weekStart, 6);
+
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleSelectingTime = () => {
+    setIsSelected((prevState) => !prevState);
+  };
+
+  const SingleCalendarDemo = () => {
+    const demoDate = new Date();
+    const [date, setDate] = useState(demoDate);
+
+    const handleOnDateSelected = (props: {
+      date: Date;
+      nextMonth: boolean;
+      prevMonth: boolean;
+      selectable: boolean;
+      selected: boolean;
+      today: boolean;
+    }) => {
+      const { date } = props;
+      if (date instanceof Date && !isNaN(date.getTime())) {
+        setDate(date);
+      }
+    };
+
+    return (
+      <Box>
+        <Box> {format(date, 'yyyy-MM-dd')}</Box>
+        <CalendarPanel
+          dayzedHookProps={{
+            showOutsideDays: true,
+            onDateSelected: handleOnDateSelected,
+            selected: date,
+            minDate: subDays(demoDate, 8),
+            maxDate: addDays(demoDate, 8),
+          }}
+          configs={{
+            dateFormat: 'yyyy-MM-dd',
+            monthNames: Month_Names_Short,
+            dayNames: Weekday_Names_Short,
+            firstDayOfWeek: 0,
+          }}
+        />
+      </Box>
+    );
+  };
+  const weekGrid = () => {
+    const days = ['Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne'];
+
+    const generateTimeSlots = () => {
+      const startHour = 7;
+      const endHour = 19;
+      const timeSlots = eachHourOfInterval({
+        start: new Date(0, 0, 0, startHour),
+        end: new Date(0, 0, 0, endHour),
+      });
+      return timeSlots.map((time) => format(time, 'HH:mm'));
+    };
+
+    return (
+      <Table variant="striped" colorScheme="teal" size="sm">
+        <Tbody>
+          {days.map((day) => (
+            <Tr key={day}>
+              <Td>{day}</Td>
+              {generateTimeSlots().map((time) => (
+                <Td key={`${day}-${time}`}>
+                  <Time
+                    time={time}
+                    onClick={handleSelectingTime}
+                    isSelected={isSelected}
+                  />
+                </Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    );
+  };
+
+  return (
+    <Box>
+      <Heading mb={4}>Choose a date</Heading>
+      <Flex direction={{ base: 'column', md: 'row' }} mb={4}>
+        <Box flex={1} mr={{ md: 4 }}>
+          <SingleCalendarDemo />
+        </Box>
+        {selectedDoctor != null && (
+          <Box flex={1}>
+            <AppointmentDoctorCard doctor={selectedDoctor} />
+          </Box>
+        )}
+      </Flex>
+      {weekGrid()}
+    </Box>
+  );
+};
+
+export default AppointmentPage;
---
+++ src/components/clinicPicker/ClinicPicker.tsx
@@ -0,0 +1,45 @@
+import { Center, Flex, Spinner, Text } from '@chakra-ui/react';
+import React from 'react';
+import { useAppContext } from '../../context/AppContext';
+import { useClinics } from '../../hooks/useClinics'; // Assuming you have a similar hook for clinics
+import ClinicCard from './ClinicCard';
+
+const ClinicPicker: React.FC = () => {
+  const { clinicList, selectedDoctor } = useAppContext();
+  const { loading, error } = useClinics(); // Assuming you have a similar hook for clinics
+
+  const getClinicsForSelectedDoctor = () => {
+    if (selectedDoctor != null && clinicList != null) {
+      return clinicList.filter((clinic) =>
+        selectedDoctor.clinics.includes(clinic.id)
+      );
+    }
+    return [];
+  };
+
+  const renderClinicCarousel = () => {
+    if (!clinicList) {
+      return null;
+    }
+
+    return getClinicsForSelectedDoctor()?.map((clinic) => (
+      <ClinicCard key={clinic.id} clinic={clinic} />
+    ));
+  };
+
+  return (
+    <Center py={6}>
+      <Flex direction="row" overflowX="scroll" py={5}>
+        {loading ? (
+          <Spinner />
+        ) : error ? (
+          <Text>{error}</Text>
+        ) : (
+          renderClinicCarousel()
+        )}
+      </Flex>
+    </Center>
+  );
+};
+
+export default ClinicPicker;

01d27aa67a1cdee690bc6f5f67c3040736b647b3
Sidonius <lach.zdenek@gmail.com>
2023-12-10 14:24:38 +0100

    Refine fetching approach for future backend integration, making it configurable via config.json and restructuring files.

--- src/hooks/useClinics.ts
+++ src/hooks/useClinics.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAppContext } from 'context/AppContext';
 import { useEffect, useState } from 'react';
-
+import config from '../../config/config.json';
 export const useClinics = () => {
   const { setClinicList } = useAppContext();
   const [loadingClinics, setLoadingClinics] = useState(true);
@@ -10,7 +10,7 @@ export const useClinics = () => {
   useEffect(() => {
     const fetchClinics = async () => {
       try {
-        const response = await axios.get('http://localhost:3001/api/clinics');
+        const response = await axios.get(config.clinicsApiUrl);
         setClinicList(response.data);
         setLoadingClinics(false);
       } catch (err: any) {

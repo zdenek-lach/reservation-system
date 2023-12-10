8012a3655f554bb7d729e145bfa684f26e6f06be
Sidonius <lach.zdenek@gmail.com>
2023-12-10 21:35:47 +0100

    A little fup fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import RootLayout from 'layouts/RootLayout';
 import { Route, RouterProvider, createRoutesFromElements } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
 import AppointmentPage from './pages/AppointmentPage';
-import ClinicPicker from './pages/ClinicPicker';
-import DoctorPicker from './pages/DoctorPicker';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 import Management from './pages/Management';
@@ -12,8 +10,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
       <Route index element={<HomePage />} />
-      <Route path="doctor-picker" element={<DoctorPicker />} />
-      <Route path="clinic-picker" element={<ClinicPicker />} />
+      {/* <Route path="doctor-picker" element={<DoctorPicker />} />
+      <Route path="clinic-picker" element={<ClinicPicker />} /> */}
       <Route path="appointment-page" element={<AppointmentPage />} />
       <Route path="management" element={<Management />} />
     </Route>

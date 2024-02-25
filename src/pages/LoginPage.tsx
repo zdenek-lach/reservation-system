ade4c85d69a5e5b55f4a3fe0ee45b3e45f191a81
markooLohn <lohn@edhouse.cz>
2024-02-26 00:30:24 +0100

    Login experiments

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,19 +1,49 @@
 import { useAppContext } from 'context/AppContext';
 import { Button, Container, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
+import config from '../../config/config.json';
+import axios from 'axios';
+import { FormEventHandler } from 'react';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const { setIsLoggedIn } = useAppContext();
-  const handleLoginButton = () => {
+  const handleLoginButton = (e:Event) => {
     //TODO: login logic
+    e.preventDefault()
+      const editUrl =
+        config.api.login;
+
+      const username = e.target[0].value;
+      const password = e.target[1].value;
+
+    const loginData = {
+      "username": username,
+      "password": password,
+    };
+
+    axios
+      .post(editUrl, loginData)
+      .then((response) => {
+        console.log(
+          `Successfully logged in`
+        );
+        console.log(response.status);
+      })
+      .catch((error) => {
+        console.error(
+          `Error while logging in:`,
+          error
+        );
+      });
+
     setIsLoggedIn(true);
     navigate('/management');
   };
   return (
     <Container className="mt-5">
       <h2>Login</h2>
-      <Form>
+      <Form id="loginForm" onSubmit={handleLoginButton}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
@@ -24,7 +54,7 @@ const LoginPage = () => {
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
 
-        <Button variant="primary" type="submit" onClick={handleLoginButton}>
+        <Button variant="primary" type="submit">
           Login
         </Button>
       </Form>

24abd0050f9aee78b476b37f8d3d87d015b76c06
markooLohn <lohn@edhouse.cz>
2024-03-01 01:21:06 +0100

    Almost got heart attack

--- src/components/WeekGrid2.tsx
+++ src/components/WeekGrid2.tsx
@@ -28,31 +28,67 @@ const WeekGrid2: React.FC<WeekGrid2Props> = ({
 }) => {
   const days = ['Po', 'Út', 'St', 'Čt', 'Pá'];
 
-  const [buttonStates, setButtonStates] = useState(() => {
-    let initialButtonStates = {};
+  const getInitialShifts = () => {
+    let initialButtonStates: string[] = [];
     initialShifts?.forEach((slot) => {
       // Use optional chaining here
-      const buttonKey = '${slot.day.getTime()}-${slot.time}';
-      initialButtonStates[buttonKey] = true;
+      const buttonKey = `${slot.day.getFullYear()}-${slot.day.getMonth()}-${slot.day.getDate()}-${
+        slot.time
+      }`;
+      initialButtonStates.push(buttonKey);
     });
     return initialButtonStates;
-  });
+  };
+
+  const [buttonStates, setButtonStates] = useState<string[]>([]);
   const [selectedTimes, setSelectedTimes] = useState(initialShifts || []);
 
-  const addOrRemoveSelectedTime = (dayDate: Date, time: string) => {
-    const buttonKey = `${dayDate.getTime()}-${time}`;
-    const isSelected = buttonStates[buttonKey];
+  const getSelectedTimes = () => {
+    let times: TimeSlot[] = [];
+    initialShifts?.forEach((slot) => {
+      times.push({ day: slot.day, time: slot.time });
+    });
+    return times;
+  };
 
-    let newButtonStates = { ...buttonStates };
+  useEffect(() => {
+    if (initialShifts) {
+      const shifts = getInitialShifts();
+      const times = getSelectedTimes();
+      setButtonStates(shifts);
+      setSelectedTimes(times);
+    }
+  }, [initialShifts]);
+
+  const getIsSelected = (buttonStates: string[], buttonKey: string) => {
+    for (const state of buttonStates) {
+      if (state == buttonKey) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const addOrRemoveSelectedTime = (dayDate: Date, time: string) => {
+    const buttonKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
+    const isSelected = getIsSelected(buttonStates, buttonKey);
+
+    let newButtonStates = [...buttonStates];
     let newSelectedTimes = [...selectedTimes];
 
     if (isSelected) {
-      newButtonStates[buttonKey] = false;
-      newSelectedTimes = newSelectedTimes.filter(
-        (slot) => slot.day.getTime() !== dayDate.getTime() || slot.time !== time
-      );
+      newButtonStates = newButtonStates.filter((state) => {
+        return state != buttonKey;
+      });
+      newSelectedTimes = newSelectedTimes.filter((slot) => {
+        const date1 = `${slot.day.getFullYear()}-${slot.day.getMonth()}-${slot.day.getDate()}-${
+          slot.time
+        }`;
+        const date2 = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
+        return date1 != date2;
+      });
     } else {
-      newButtonStates[buttonKey] = true;
+      newButtonStates.push(buttonKey);
       newSelectedTimes.push({ day: dayDate, time });
     }
 
@@ -64,7 +100,7 @@ const WeekGrid2: React.FC<WeekGrid2Props> = ({
     setClickedButtons(selectedTimes);
   }, [selectedTimes, setClickedButtons]);
   return (
-    <Table striped size="md">
+    <Table striped size='md'>
       <tbody>
         {days.map((day, index) => {
           const dayDate = new Date(
@@ -77,19 +113,19 @@ const WeekGrid2: React.FC<WeekGrid2Props> = ({
             0 // Sets the time to 00:00:00.000
           );
           return (
-            <tr key={day} className="bg-light text-dark">
+            <tr key={day} className='bg-light text-dark'>
               <td>
                 {day}
-                <Badge className="">
+                <Badge className=''>
                   {dayDate.toLocaleDateString('cs-CZ')}
                 </Badge>
               </td>
               {timeSlots.map((time) => {
-                const buttonKey = `${dayDate.getTime()}-${time}`; // Use getTime() instead of getDate()
-                const isSelected = buttonStates[buttonKey];
+                const buttonKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
+                const isSelected = getIsSelected(buttonStates, buttonKey);
 
                 return (
-                  <td key={buttonKey} className="p-2">
+                  <td key={buttonKey} className='p-2'>
                     <Button
                       style={{
                         borderRadius: '20px',
@@ -98,9 +134,8 @@ const WeekGrid2: React.FC<WeekGrid2Props> = ({
                       onClick={() =>
                         !readOnly && addOrRemoveSelectedTime(dayDate, time)
                       }
-                      // disabled={readOnly}
                     >
-                      {time}
+                      {time} - {dayDate.toLocaleDateString()}
                     </Button>
                   </td>
                 );

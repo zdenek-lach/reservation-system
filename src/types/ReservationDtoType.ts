af97bd9b029cf62a6fc9087068712e0067dcdd7f
markooLohn <lohn@edhouse.cz>
2024-03-01 12:55:34 +0100

    docker file update

--- src/types/ReservationDtoType.ts
+++ src/types/ReservationDtoType.ts
@@ -2,14 +2,14 @@ import Clinic from './ClinicType';
 import Doctor from './DoctorType';
 
 interface ReservationDto {
-    employeeId: string;
-    clinicId: string;
-    date: string;
-    timeFrom: string;
-    note: string;
-    name: string;
-    surname: string
-    email: string;
-    phone: string;
+  employeeId: number;
+  clinicId: string;
+  date: string;
+  timeFrom: string;
+  note: string;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
 }
 export default ReservationDto;

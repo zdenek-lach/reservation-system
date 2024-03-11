532c31ad60f5b129d4edc0624fafbcff348aad20
markooLohn <lohn@edhouse.cz>
2024-03-11 21:22:12 +0100

    Improved UI on Login page and UI

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAppContext } from 'context/AppContext';
 import { useState } from 'react';
-import { Alert, Button, Container, Form } from 'react-bootstrap';
+import { Alert, Button, Container, Form, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
 import config from '../../config/config.json';
 import { login } from './../security/AuthService';
@@ -59,32 +59,43 @@ const LoginPage = () => {
   };
 
   return (
-    <Container className="mt-5">
-      <h2>Přihlášení</h2>
-      <Form id="loginForm" onSubmit={handleLoginButton}>
-        {showLoginError && <Alert variant="danger">{loginError}</Alert>}
-        <Form.Group controlId="formUsername">
-          <Form.Label>Uživatelské jméno</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Uživatelské jméno"
-            value={loginUserName}
-            onChange={(e) => setLoginUserName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Heslo</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Heslo"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={handleLoginButton}>
-          Login
-        </Button>
-      </Form>
+    <Container className='mt-5 ps-'>
+      <Row>
+        <Col></Col>
+        <Col>
+          <h2>Přihlášení</h2>
+          <Form id='loginForm' onSubmit={handleLoginButton}>
+            {showLoginError && <Alert variant='danger'>{loginError}</Alert>}
+            <Form.Group controlId='formUsername'>
+              <Form.Label>Uživatelské jméno</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Uživatelské jméno'
+                value={loginUserName}
+                onChange={(e) => setLoginUserName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='formPassword'>
+              <Form.Label>Heslo</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Heslo'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button
+              variant='danger'
+              type='submit'
+              onClick={handleLoginButton}
+              className='mt-2'
+            >
+              Přihlásit se
+            </Button>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
     </Container>
   );
 };

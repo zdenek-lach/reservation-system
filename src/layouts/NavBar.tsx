4ced37c289876a69c23843a34aa0caca0d108598
Sidonius <lach.zdenek@gmail.com>
2024-03-19 21:35:58 +0100

    Not intrusive API Tester reimplementation.

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -1,6 +1,7 @@
+import ApiTester from 'components/ApiTester';
 import { useAppContext } from 'context/AppContext';
-import React from 'react';
-import { Button, Container, Image, Nav, Navbar } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Container, Form, Image, Nav, Navbar } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 import packageJson from '../../package.json';
 import logoText from '../assets/logoText.png';
@@ -13,57 +14,77 @@ const NavBar: React.FC = () => {
   const handleLogout = () => {
     setIsLoggedIn(false);
   };
+  const [insanity, setInsanity] = useState(false);
 
+  const handleSanityToggle = () => {
+    setInsanity(!insanity);
+  };
   const navigate = useNavigate();
 
   return (
     <Navbar
-      variant='dark'
-      expand='lg'
-      className='p-3 border-bottom border-danger'
+      variant="dark"
+      expand="lg"
+      className="p-3 border-bottom border-danger"
     >
       <Container>
         <Navbar.Brand
           onClick={() => {
             navigate('/');
           }}
-          className='d-flex align-items-center'
+          className="d-flex align-items-center"
         >
-        <div className='secondary'>
-            <Image src={logoText} width={250} className='p-0 m-0' />
+          <div className="secondary">
+            <Image src={logoText} width={250} className="p-0 m-0" />
           </div>
         </Navbar.Brand>
+        <Nav.Item className="p-2 d-flex align-items-center">
+          <Form>
+            <Form.Check
+              type="switch"
+              id="custom-switch"
+              label="My sanity is running low"
+              onChange={handleSanityToggle} // Add the onChange handler
+            />
+          </Form>
+        </Nav.Item>
+        {insanity && (
+          <>
+            <Nav.Item>
+              <ApiTester />
+            </Nav.Item>
+          </>
+        )}
         <Navbar.Toggle />
-        <Navbar.Collapse className='justify-content-end'>
+        <Navbar.Collapse className="justify-content-end">
           <Nav>
-            <Nav.Item className=' p-2 d-flex align-items-center'>
+            <Nav.Item className=" p-2 d-flex align-items-center">
               verze {packageJson.version}
             </Nav.Item>
 
             {isLoggedIn ? (
               <>
                 <Button
-                  className='me-2'
-                  variant='outline-danger'
+                  className="me-2"
+                  variant="outline-danger"
                   onClick={() => navigate('/management')}
-                  
                 >
                   Management
                 </Button>
                 <Button
-                  href='/'
+                  href="/"
                   onClick={handleLogout}
-                  variant='outline-danger'
-                  className='d-flex align-items-center'
+                  variant="outline-danger"
+                  className="d-flex align-items-center"
                 >
                   Odhlásit se
                 </Button>
               </>
             ) : (
               <Button
-                className='d-flex align-items-center'
-                href='/login'
-                variant='danger'
+                className="d-flex align-items-center"
+                href="/login"
+                variant="danger"
               >
                 Přihlásit se
               </Button>

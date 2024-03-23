a53461530c2559f4e245ad0727617ace6f67ea15
Sidonius <lach.zdenek@gmail.com>
2024-03-23 20:10:20 +0100

    Preset implementation

--- src/components/PresetSelector.tsx
+++ src/components/PresetSelector.tsx
@@ -3,6 +3,7 @@ import { useAppContext } from 'context/AppContext';
 import { useEffect, useState } from 'react';
 import { Button, Dropdown, Form, InputGroup } from 'react-bootstrap';
 import { authHeader } from 'security/AuthService';
+import PresetType from 'types/PresetType';
 import config from '../../config/config.json';
 import { TimeSlot } from './WeekGrid2';
 
@@ -11,15 +12,43 @@ type PresetSelectorProps = {
   setPresetName?: (value: string) => void;
   clickedButtons: TimeSlot[];
 };
-//TODO add null preset on the top of the list (labeled NEW PRESET or smt)
-//fix adding the preset formatting (2 buttons lmao)
+
 const PresetSelector: React.FC<PresetSelectorProps> = ({
   presetName,
   setPresetName,
   clickedButtons,
 }) => {
-  const { selectedPreset, setSelectedPreset } = useAppContext();
+  const {
+    setShowMessageToast,
+    selectedPreset,
+    setSelectedPreset,
+    selectedDoctor,
+  } = useAppContext();
   const [presetList, setPresetList] = useState([]);
+
+  const fetchPresets = async () => {
+    try {
+      const response = await axios.get(config.api.presetsApi.list, {
+        headers: {
+          ...authHeader(),
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 200) {
+        console.log('Retrieving presets was successful');
+        setPresetList(response.data);
+      } else {
+        console.log('You have caused an error!');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPresets();
+  }, []);
 
   const deleteSelectedPreset = () => {
     axios
@@ -31,6 +60,7 @@ const PresetSelector: React.FC<PresetSelectorProps> = ({
         console.log(response.status);
 
         setSelectedPreset(null);
+        fetchPresets();
       })
       .catch((error) => {
         console.error(`Error deleting preset ${selectedPreset.id}:`, error);
@@ -38,10 +68,58 @@ const PresetSelector: React.FC<PresetSelectorProps> = ({
   };
 
   const editSelectedPreset = () => {
+    // Create a new object based on the current selectedPreset
+    const updatedPreset = { ...selectedPreset };
+
+    // Reset the days
+    updatedPreset.monday = [];
+    updatedPreset.tuesday = [];
+    updatedPreset.wednesday = [];
+    updatedPreset.thursday = [];
+    updatedPreset.friday = [];
+    updatedPreset.saturday = [];
+    updatedPreset.sunday = [];
+
+    // Populate the days based on the clickedButtons
+    clickedButtons.forEach((button) => {
+      const dayOfWeek = new Date(button.day).toLocaleString('en-US', {
+        weekday: 'long',
+      });
+
+      switch (dayOfWeek.toLowerCase()) {
+        case 'monday':
+          updatedPreset.monday.push(button.time);
+          break;
+        case 'tuesday':
+          updatedPreset.tuesday.push(button.time);
+          break;
+        case 'wednesday':
+          updatedPreset.wednesday.push(button.time);
+          break;
+        case 'thursday':
+          updatedPreset.thursday.push(button.time);
+          break;
+        case 'friday':
+          updatedPreset.friday.push(button.time);
+          break;
+        case 'saturday':
+          updatedPreset.saturday.push(button.time);
+          break;
+        case 'sunday':
+          updatedPreset.sunday.push(button.time);
+          break;
+        default:
+          console.log(
+            `Day ${dayOfWeek} is not accounted for in the Preset object.`
+          );
+      }
+    });
+
+    // Send the updatedPreset in the PUT request
     axios
       .put(
         config.api.presetsApi.edit + `/${selectedPreset.id}`,
-        selectedPreset,
+        updatedPreset,
         {
           headers: { ...authHeader() },
         }
@@ -49,6 +127,7 @@ const PresetSelector: React.FC<PresetSelectorProps> = ({
       .then((response) => {
         console.log(`Successfully edited preset ${selectedPreset.id}`);
         console.log(response.status);
+        fetchPresets();
       })
       .catch((error) => {
         console.error(`Error editing preset ${selectedPreset.id}:`, error);
@@ -114,6 +193,7 @@ const PresetSelector: React.FC<PresetSelectorProps> = ({
       if (response.status === 200) {
         console.log('Adding preset was successful');
         setSelectedPreset(newPreset);
+        fetchPresets();
       } else {
         console.log('You have caused an error!');
       }
@@ -121,46 +201,22 @@ const PresetSelector: React.FC<PresetSelectorProps> = ({
       console.log(error);
     }
   };
-
-  useEffect(() => {
-    const fetchPresets = async () => {
-      try {
-        const response = await axios.get(config.api.presetsApi.list, {
-          headers: {
-            ...authHeader(),
-            'Content-Type': 'application/json',
-          },
-        });
-
-        if (response.status === 200) {
-          console.log('Retrieving presets was successful');
-          setPresetList(response.data);
-        } else {
-          console.log('You have caused an error!');
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchPresets();
-  }, []);
-
+//todo after new preset, update the dropdown-selected one .. or the selected one overall
   return (
     <label>
       <Dropdown>
-        <Dropdown.Toggle variant="success" className="me-2 mt-3 mb-3">
+        <Dropdown.Toggle variant='success' className='me-2 mt-3 mb-3'>
           {selectedPreset != null
             ? `Preset ${selectedPreset.name}`
             : 'Vyberte Preset'}
         </Dropdown.Toggle>
         {selectedPreset != null && (
-          <InputGroup className="mb-3">
+          <InputGroup className='mb-3'>
             <Form.Control
-              placeholder="Jméno presetu"
-              aria-label="Jméno presetu"
-              aria-describedby="preset-name"
-              value={selectedPreset.name}
+              placeholder='Jméno presetu'
+              aria-label='Jméno presetu'
+              aria-describedby='preset-name'
+              value={selectedPreset.name.toString()}
               onChange={(e) => {
                 setPresetName(e.target.value);
                 setSelectedPreset({ ...selectedPreset, name: e.target.value });
@@ -170,31 +226,34 @@ const PresetSelector: React.FC<PresetSelectorProps> = ({
         )}
         {selectedPreset != null && (
           <>
-            <Button variant="danger" onClick={deleteSelectedPreset}>
+            <Button variant='danger' onClick={deleteSelectedPreset}>
               Odstranit vybraný preset
             </Button>
-            <Button variant="warning" onClick={editSelectedPreset}>
+            <Button variant='warning' onClick={editSelectedPreset}>
               Edit selected preset
             </Button>
           </>
         )}
         {selectedPreset == null && (
           <>
-            <InputGroup className="mb-3">
+            <InputGroup className='mb-3'>
               <Form.Control
-                placeholder="Jméno presetu"
-                aria-label="Jméno presetu"
-                aria-describedby="preset-name"
+                placeholder='Jméno presetu'
+                aria-label='Jméno presetu'
+                aria-describedby='preset-name'
                 value={presetName || ''}
                 onChange={(e) => setPresetName(e.target.value)}
               />
             </InputGroup>
-            <Button variant="primary" onClick={submitNewPreset}>
+            <Button variant='primary' onClick={submitNewPreset}>
               Add new preset
             </Button>
           </>
         )}
         <Dropdown.Menu>
+          <Dropdown.Item onClick={() => setSelectedPreset(null)}>
+            Nový Preset
+          </Dropdown.Item>
           {presetList &&
             presetList.map((preset) => (
               <Dropdown.Item

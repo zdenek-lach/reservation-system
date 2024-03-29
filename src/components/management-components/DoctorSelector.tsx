95669ff8edad98943cb2773414294fe38ec2ea0e
Sidonius <lach.zdenek@gmail.com>
2024-03-29 11:50:44 +0100

    .

--- src/components/management-components/DoctorSelector.tsx
+++ src/components/management-components/DoctorSelector.tsx
@@ -5,65 +5,66 @@ import { fetchLoggedDoctor } from 'security/AuthService';
 import Doctor from 'types/DoctorType';
 
 interface DoctorSelectorProps {
-  selectedDoctorProp?: Doctor | null;
-  setSelectedDoctorProp?: Dispatch<SetStateAction<Doctor | null>>;
+	selectedDoctorProp?: Doctor | null;
+	setSelectedDoctorProp?: Dispatch<SetStateAction<Doctor | null>>;
 }
 
 const DoctorSelector: React.FC<DoctorSelectorProps> = ({
-  selectedDoctorProp,
-  // : selectedDoctor
-  setSelectedDoctorProp,
-  // : setSelectedDoctor
+	selectedDoctorProp,
+	// : selectedDoctor
+	setSelectedDoctorProp,
+	// : setSelectedDoctor
 }) => {
-  const {
-    doctorList,
-    selectedDoctor: contextSelectedDoctor,
-    setSelectedDoctor: contextSetSelectedDoctor,
-    loggedInDoctor,
-    setLoggedInDoctor,
-  } = useAppContext();
+	const {
+		doctorList,
+		selectedDoctor: contextSelectedDoctor,
+		setSelectedDoctor: contextSetSelectedDoctor,
+		loggedInDoctor,
+		setLoggedInDoctor,
+	} = useAppContext();
 
-  useEffect(() => {
-    fetchLoggedDoctor().then((doctor) => {
-      if (doctor != null) {
-        setLoggedInDoctor(doctor);
-      } else {
-        console.error('Failed to fetch logged doctor!');
-      }
-    });
-  }, []);
+	useEffect(() => {
+		fetchLoggedDoctor().then((doctor) => {
+			if (doctor != null) {
+				setLoggedInDoctor(doctor);
+			} else {
+				console.error('Failed to fetch logged doctor!');
+			}
+		});
+	}, []);
 
-  return (
-    <>
-      {contextSelectedDoctor === null ||
-        (loggedInDoctor != null && (
-          <label>
-            <Dropdown>
-              <Dropdown.Toggle variant='success' className='me-3 mt-3 mb-3'>
-                {loggedInDoctor != null
-                  ? `${loggedInDoctor.title} ${loggedInDoctor.firstName} ${loggedInDoctor.lastName}`
-                  : 'Vyberte spravovaného doktora'}
-              </Dropdown.Toggle>
+	return (
+		<>
+			{contextSelectedDoctor === null ||
+				(loggedInDoctor === null && (
+					<label>
+						<Dropdown>
+							<Dropdown.Toggle
+								variant='success'
+								className='me-3 mt-3 mb-3'>
+								{loggedInDoctor != null
+									? `${loggedInDoctor.title} ${loggedInDoctor.firstName} ${loggedInDoctor.lastName}`
+									: 'Vyberte spravovaného doktora'}
+							</Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                {doctorList &&
-                  doctorList.map((doctor) => (
-                    <Dropdown.Item
-                      key={doctor.id}
-                      onClick={() => {
-                        contextSetSelectedDoctor(doctor),
-                          setLoggedInDoctor(doctor);
-                      }}
-                    >
-                      {`${doctor.title} ${doctor.firstName} ${doctor.lastName}`}
-                    </Dropdown.Item>
-                  ))}
-              </Dropdown.Menu>
-            </Dropdown>
-          </label>
-        ))}
-    </>
-  );
+							<Dropdown.Menu>
+								{doctorList &&
+									doctorList.map((doctor) => (
+										<Dropdown.Item
+											key={doctor.id}
+											onClick={() => {
+												contextSetSelectedDoctor(doctor),
+													setLoggedInDoctor(doctor);
+											}}>
+											{`${doctor.title} ${doctor.firstName} ${doctor.lastName}`}
+										</Dropdown.Item>
+									))}
+							</Dropdown.Menu>
+						</Dropdown>
+					</label>
+				))}
+		</>
+	);
 };
 
 export default DoctorSelector;

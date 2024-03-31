fd56c0b89a5ee884e8314ab861a5ec52858a109b
Sidonius <lach.zdenek@gmail.com>
2024-03-31 18:22:32 +0200

    Fixed LoginPage see password mode and my reservations reset filter button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,122 +3,131 @@ import FooterManagement from 'components/management-components/FooterManagement'
 import { useAppContext } from 'context/AppContext';
 import { Fragment, useState } from 'react';
 import { Alert, Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { EyeFill, EyeSlashFill } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router';
 import config from '../../config/config.json';
 import { login } from './../security/AuthService';
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-  const { setIsLoggedIn, username, setUsername } = useAppContext();
-  const { setTimerSet } = useAppContext();
-  const [loginUserName, setLoginUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [loginError, setLoginError] = useState('');
-  const [showLoginError, setShowLoginError] = useState(false);
+	const navigate = useNavigate();
+	const { setIsLoggedIn, username, setUsername } = useAppContext();
+	const { setTimerSet } = useAppContext();
+	const [loginUserName, setLoginUserName] = useState('');
+	const [password, setPassword] = useState('');
+	const [loginError, setLoginError] = useState('');
+	const [showLoginError, setShowLoginError] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
 
-  const handleLoginButton = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+	const handleLoginButton = (e: { preventDefault: () => void }) => {
+		e.preventDefault();
 
-    const loginData = {
-      username: loginUserName,
-      password: password,
-    };
+		const loginData = {
+			username: loginUserName,
+			password: password,
+		};
 
-    axios
-      .post(config.api.authApi.getToken, loginData)
-      .then((response) => {
-        if (response.status === 200) {
-          console.log(`Successfully logged in`);
-          console.log(response.status);
-          login(loginUserName, password, setIsLoggedIn);
-          setTimerSet(true);
-          setIsLoggedIn(true);
-          setUsername(loginUserName);
-          navigate('/management');
-        } else if (response.status === 400) {
-          setLoginError(response.data.message);
-          setShowLoginError(true);
-        }
-      })
-      .catch((error) => {
-        console.error(`Error while logging in:`, error);
+		axios
+			.post(config.api.authApi.getToken, loginData)
+			.then((response) => {
+				if (response.status === 200) {
+					console.log(`Successfully logged in`);
+					console.log(response.status);
+					login(loginUserName, password, setIsLoggedIn);
+					setTimerSet(true);
+					setIsLoggedIn(true);
+					setUsername(loginUserName);
+					navigate('/management');
+				} else if (response.status === 400) {
+					setLoginError(response.data.message);
+					setShowLoginError(true);
+				}
+			})
+			.catch((error) => {
+				console.error(`Error while logging in:`, error);
 
-        if (error.response) {
-          const statusCode = error.response.status;
+				if (error.response) {
+					const statusCode = error.response.status;
 
-          if (statusCode === 400) {
-            setLoginError('Chybí uživatelské jméno nebo heslo');
-          } else if (statusCode === 401) {
-            setLoginError('Neplatné přihlašovací údaje.');
-          } else {
-            setLoginError('Nastala chyba při přihlášení.');
-          }
-        } else {
-          setLoginError('Nastala chyba při komunikaci se serverem.');
-        }
+					if (statusCode === 400) {
+						setLoginError('Chybí uživatelské jméno nebo heslo');
+					} else if (statusCode === 401) {
+						setLoginError('Neplatné přihlašovací údaje.');
+					} else {
+						setLoginError('Nastala chyba při přihlášení.');
+					}
+				} else {
+					setLoginError('Nastala chyba při komunikaci se serverem.');
+				}
 
-        setShowLoginError(true);
-      });
-  };
+				setShowLoginError(true);
+			});
+	};
 
-  return (
-    <Fragment>
-      <Container className='mt-5 ps-'>
-        <Row>
-          <Col></Col>
-          <Col>
-            <Row>
-              <h2 className='ps-0'>Přihlášení</h2>
-            </Row>
-            <Form id='loginForm' onSubmit={handleLoginButton}>
-              {showLoginError && <Alert variant='danger'>{loginError}</Alert>}
-              <Form.Group controlId='formUsername' className='mt-4'>
-                <Row>
-                  <Form.Control
-                    type='text'
-                    placeholder='Uživatelské jméno'
-                    value={loginUserName}
-                    onChange={(e) => setLoginUserName(e.target.value)}
-                  />
-                </Row>
-              </Form.Group>
-              <Form.Group controlId='formPassword' className='mt-4'>
-                <Row>
-                  <Col className='ps-0 pe-0 col-9'>
-                    <Form.Control
-                      type='password'
-                      placeholder='Heslo'
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </Col>
-                  <Col className='ps-0 pe-0'>
-                    <div className='d-grid'>
-                      <Button className='' variant='danger' type='button'>
-                        Zobrazit
-                      </Button>
-                    </div>
-                  </Col>
-                </Row>
-              </Form.Group>
-              <Row>
-                <Button
-                  variant='danger'
-                  type='submit'
-                  onClick={handleLoginButton}
-                  className='mt-4 col-3'
-                >
-                  Přihlásit se
-                </Button>
-              </Row>
-            </Form>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
-      <FooterManagement></FooterManagement>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Container className='mt-5 ps-'>
+				<Row>
+					<Col></Col>
+					<Col>
+						<Row>
+							<h2 className='ps-0'>Přihlášení</h2>
+						</Row>
+						<Form
+							id='loginForm'
+							onSubmit={handleLoginButton}>
+							{showLoginError && <Alert variant='danger'>{loginError}</Alert>}
+							<Form.Group
+								controlId='formUsername'
+								className='mt-4'>
+								<Row>
+									<Form.Control
+										type='text'
+										placeholder='Uživatelské jméno'
+										value={loginUserName}
+										onChange={(e) => setLoginUserName(e.target.value)}
+									/>
+								</Row>
+							</Form.Group>
+							<Form.Group
+								controlId='formPassword'
+								className='mt-4'>
+								<Row>
+									<Col className='ps-0 pe-0 col-9'>
+										<Form.Control
+											type={showPassword ? 'text' : 'password'} // change type based on showPassword state
+											placeholder='Heslo'
+											value={password}
+											onChange={(e) => setPassword(e.target.value)}
+										/>
+									</Col>
+									<Col className='ps-0 pe-0'>
+										<div className='d-grid'>
+											<Button
+												variant='danger'
+												onClick={() => setShowPassword(!showPassword)}>
+												{showPassword ? <EyeSlashFill /> : <EyeFill />}
+											</Button>
+										</div>
+									</Col>
+								</Row>
+							</Form.Group>
+							<Row>
+								<Button
+									variant='danger'
+									type='submit'
+									onClick={handleLoginButton}
+									className='mt-4 col-3'>
+									Přihlásit se
+								</Button>
+							</Row>
+						</Form>
+					</Col>
+					<Col></Col>
+				</Row>
+			</Container>
+			<FooterManagement />
+		</Fragment>
+	);
 };
 
 export default LoginPage;
--- src/pages/MyReservations.tsx
+++ src/pages/MyReservations.tsx
@@ -6,15 +6,12 @@ import ClinicSelector from 'components/management-components/ClinicSelector';
 import EditReservation from 'components/management-components/EditReservation';
 import FooterManagement from 'components/management-components/FooterManagement';
 import { useAppContext } from 'context/AppContext';
-import { useClinics } from 'hooks/useClinics';
-import { useDoctors } from 'hooks/useDoctors';
-import { useReservations } from 'hooks/useReservations';
 import { Fragment, useEffect, useState } from 'react';
 import { Button, Container, Form, Modal, Table } from 'react-bootstrap';
 import {
-  ArrowCounterclockwise,
-  InfoCircle,
-  Trash3Fill,
+	ArrowCounterclockwise,
+	InfoCircle,
+	Trash3Fill,
 } from 'react-bootstrap-icons';
 import { authHeader, fetchLoggedDoctor } from 'security/AuthService';
 import { CenterSpinner } from 'styles/StyledComponentsLib';
@@ -24,280 +21,281 @@ import config from '../../config/config.json';
 import Doctor from './../types/DoctorType';
 
 const MyReservations = () => {
-  const { reservationsList, setReservationsList, currentWeek, setCurrentWeek } =
-    useAppContext();
+	const { reservationsList, setReservationsList, currentWeek, setCurrentWeek } =
+		useAppContext();
 
-  const [showInfoModal, setShowInfoModal] = useState(false);
-  const [loggedInDoctor, setLoggedInDoctor] = useState<Doctor | null>(null);
+	const [showInfoModal, setShowInfoModal] = useState(false);
+	const [loggedInDoctor, setLoggedInDoctor] = useState<Doctor | null>(null);
 
-  const [filterClinic, setFilterClinic] = useState<Clinic | null>(null);
-  const [isWeekFilterEnabled, setIsWeekFilterEnabled] = useState(false);
-  const [loading, setLoading] = useState(true);
+	const [filterClinic, setFilterClinic] = useState<Clinic | null>(null);
+	const [isWeekFilterEnabled, setIsWeekFilterEnabled] = useState(false);
+	const [loading, setLoading] = useState(true);
 
-  const [selectedReservation, setSelectedReservation] =
-    useState<Reservation | null>(null);
+	const [selectedReservation, setSelectedReservation] =
+		useState<Reservation | null>(null);
 
-  const [searchTerm, setSearchTerm] = useState('');
+	const [searchTerm, setSearchTerm] = useState('');
 
-  const handleShowInfoModal = (reservation: Reservation) => {
-    setSelectedReservation(reservation);
-    setShowInfoModal(true);
-  };
+	const handleShowInfoModal = (reservation: Reservation) => {
+		setSelectedReservation(reservation);
+		setShowInfoModal(true);
+	};
 
-  const handleCloseModal = () => {
-    setSelectedReservation(null);
-    setShowInfoModal(false);
-  };
+	const handleCloseModal = () => {
+		setSelectedReservation(null);
+		setShowInfoModal(false);
+	};
 
-  const handleDeleteReservation = (reservation: Reservation) => {
-    const deleteUrl = config.api.reservationsApi.delete + `/${reservation.id}`;
+	const handleDeleteReservation = (reservation: Reservation) => {
+		const deleteUrl = config.api.reservationsApi.delete + `/${reservation.id}`;
 
-    axios
-      .delete(deleteUrl, {
-        headers: { ...authHeader() },
-      })
-      .then((response) => {
-        console.log(`Successfully deleted reservation ${reservation.id}`);
-        console.log(response.status);
+		axios
+			.delete(deleteUrl, {
+				headers: { ...authHeader() },
+			})
+			.then((response) => {
+				console.log(`Successfully deleted reservation ${reservation.id}`);
+				console.log(response.status);
 
-        if (reservationsList) {
-          const updatedReservations = reservationsList.filter(
-            (res) => res.id !== reservation.id
-          );
-          setReservationsList(updatedReservations);
-        }
-      })
-      .catch((error) => {
-        console.error(`Error deleting reservation ${reservation.id}:`, error);
-      });
-  };
+				if (reservationsList) {
+					const updatedReservations = reservationsList.filter(
+						(res) => res.id !== reservation.id
+					);
+					setReservationsList(updatedReservations);
+				}
+			})
+			.catch((error) => {
+				console.error(`Error deleting reservation ${reservation.id}:`, error);
+			});
+	};
 
-  const startOfWeek = (date: Date) => {
-    const result = new Date(date);
-    result.setDate(result.getDate() - result.getDay() + 1);
-    return result;
-  };
+	const startOfWeek = (date: Date) => {
+		const result = new Date(date);
+		result.setDate(result.getDate() - result.getDay() + 1);
+		return result;
+	};
 
-  const endOfWeek = (date: Date) => {
-    const result = new Date(date);
-    result.setDate(result.getDate() - result.getDay() + 7);
-    return result;
-  };
+	const endOfWeek = (date: Date) => {
+		const result = new Date(date);
+		result.setDate(result.getDate() - result.getDay() + 7);
+		return result;
+	};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetchLoggedDoctor();
-        setLoggedInDoctor(response);
-        setLoading(false);
-      } catch (error) {
-        console.error('Failed to fetch user:', error);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetchLoggedDoctor();
+				setLoggedInDoctor(response);
+				setLoading(false);
+			} catch (error) {
+				console.error('Failed to fetch user:', error);
+			}
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  if (loading) {
-    console.log('Waiting for logged-in api');
-    return <CenterSpinner />;
-  }
-  return (
-    <Fragment>
-      <Container
-        style={{
-          backgroundColor: 'rgba(0, 0, 0, 0)',
-          padding: '30px',
-          alignContent: 'center',
-        }}
-      >
-        <Form.Group>
-          <Form.Control
-            type='text'
-            placeholder='Vyhledat'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <ClinicSelector
-            selectedClinic={filterClinic}
-            setSelectedClinic={setFilterClinic}
-          />
-          <Button
-            variant='danger'
-            onClick={() => {
-              setSearchTerm('');
-              setLoggedInDoctor(null);
-              setFilterClinic(null);
-            }}
-          >
-            <ArrowCounterclockwise />
-          </Button>
-          <Form.Check
-            type='checkbox'
-            label='Filtrovat dle týdnu'
-            checked={isWeekFilterEnabled}
-            onChange={(e) => setIsWeekFilterEnabled(e.target.checked)}
-          />
-          {isWeekFilterEnabled && (
-            <WeekPicker
-              currentWeek={currentWeek}
-              setCurrentWeek={setCurrentWeek}
-            />
-          )}
-        </Form.Group>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Číslo</th>
-              <th>Jméno</th>
-              <th>Příjmení</th>
-              <th>Datum a čas</th>
-              <th>Doktor</th>
-              <th>Ambulance</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody
-            style={{
-              backgroundColor: 'white',
-              padding: '20px',
-              borderRadius: '15px',
-            }}
-          >
-            {reservationsList != null &&
-              reservationsList
-                .filter(
-                  (reservation) =>
-                    reservation.client.phoneNumber.includes(searchTerm) ||
-                    reservation.client.firstName
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase()) ||
-                    reservation.client.lastName
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase()) ||
-                    reservation.client.email
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase()) ||
-                    reservation.note
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase())
-                )
-                .filter(
-                  (reservation) =>
-                    loggedInDoctor == null ||
-                    reservation.doctor.id === loggedInDoctor.id
-                )
-                .filter(
-                  (reservation) =>
-                    filterClinic == null ||
-                    reservation.clinic.id === filterClinic.id
-                )
-                .filter(
-                  (reservation) =>
-                    !isWeekFilterEnabled ||
-                    (new Date(reservation.date) >= startOfWeek(currentWeek) &&
-                      new Date(reservation.date) <= endOfWeek(currentWeek))
-                )
+	if (loading) {
+		console.log('Waiting for logged-in api');
+		return <CenterSpinner />;
+	}
+	return (
+		<Fragment>
+			<Container
+				style={{
+					backgroundColor: 'rgba(0, 0, 0, 0)',
+					padding: '30px',
+					alignContent: 'center',
+				}}>
+				<Form.Group>
+					<Form.Control
+						type='text'
+						placeholder='Vyhledat'
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+					<ClinicSelector
+						selectedClinic={filterClinic}
+						setSelectedClinic={setFilterClinic}
+					/>
+					<Button
+						variant='danger'
+						onClick={() => {
+							setSearchTerm('');
+							setFilterClinic(null);
+						}}>
+						<ArrowCounterclockwise />
+					</Button>
+					<Form.Check
+						type='checkbox'
+						label='Filtrovat dle týdnu'
+						checked={isWeekFilterEnabled}
+						onChange={(e) => setIsWeekFilterEnabled(e.target.checked)}
+					/>
+					{isWeekFilterEnabled && (
+						<WeekPicker
+							currentWeek={currentWeek}
+							setCurrentWeek={setCurrentWeek}
+						/>
+					)}
+				</Form.Group>
+				<Table
+					striped
+					bordered
+					hover>
+					<thead>
+						<tr>
+							<th>Číslo</th>
+							<th>Jméno</th>
+							<th>Příjmení</th>
+							<th>Datum a čas</th>
+							<th>Doktor</th>
+							<th>Ambulance</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody
+						style={{
+							backgroundColor: 'white',
+							padding: '20px',
+							borderRadius: '15px',
+						}}>
+						{reservationsList != null &&
+							reservationsList
+								.filter(
+									(reservation) =>
+										reservation.client.phoneNumber.includes(searchTerm) ||
+										reservation.client.firstName
+											.toLowerCase()
+											.includes(searchTerm.toLowerCase()) ||
+										reservation.client.lastName
+											.toLowerCase()
+											.includes(searchTerm.toLowerCase()) ||
+										reservation.client.email
+											.toLowerCase()
+											.includes(searchTerm.toLowerCase()) ||
+										reservation.note
+											.toLowerCase()
+											.includes(searchTerm.toLowerCase())
+								)
+								.filter(
+									(reservation) =>
+										loggedInDoctor == null ||
+										reservation.doctor.id === loggedInDoctor.id
+								)
+								.filter(
+									(reservation) =>
+										filterClinic == null ||
+										reservation.clinic.id === filterClinic.id
+								)
+								.filter(
+									(reservation) =>
+										!isWeekFilterEnabled ||
+										(new Date(reservation.date) >= startOfWeek(currentWeek) &&
+											new Date(reservation.date) <= endOfWeek(currentWeek))
+								)
 
-                .map((reservation) => (
-                  <tr key={reservation.id}>
-                    <td>{reservation.id}</td>
-                    <td>{reservation.client.firstName}</td>
-                    <td>{reservation.client.lastName}</td>
-                    <td>
-                      {getFormattedDate(new Date(reservation.date))}
-                      {' v '}
-                      {reservation.time}
-                    </td>
-                    <td>
-                      {reservation.doctor.firstName}{' '}
-                      {reservation.doctor.lastName}
-                    </td>
-                    <td>{reservation.clinic.name}</td>
-                    <td>
-                      <Button
-                        variant='info'
-                        size='lg'
-                        className='mr-1 me-1'
-                        onClick={() => handleShowInfoModal(reservation)}
-                      >
-                        <InfoCircle />
-                      </Button>
-                      <EditReservation
-                        Reservation={reservation as Reservation}
-                        ReservationList={reservationsList}
-                        SetReservationList={setReservationsList}
-                      />
-                      <Button
-                        variant='danger'
-                        size='lg'
-                        onClick={() => handleDeleteReservation(reservation)}
-                      >
-                        <Trash3Fill />
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-          </tbody>
-        </Table>
-        <AddReservation managementMode={true} />
-        <Modal show={showInfoModal} onHide={handleCloseModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Informace o rezervaci</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {selectedReservation && (
-              <div>
-                <p>
-                  <strong>Číslo:</strong> {selectedReservation.id}
-                </p>
-                <p>
-                  <strong>Jméno:</strong> {selectedReservation.client.firstName}
-                </p>
-                <p>
-                  <strong>Příjmení:</strong>{' '}
-                  {selectedReservation.client.lastName}
-                </p>
-                <p>
-                  <strong>Telefon:</strong>
-                  {selectedReservation.client.phoneNumber}
-                </p>
-                <p>
-                  <strong>E-mail:</strong> {selectedReservation.client.email}
-                </p>
-                <p>
-                  <strong>Datum: </strong>
-                  {getFormattedDate(new Date(selectedReservation.date))}
-                </p>
-                <p>
-                  <strong>Čas:</strong> {selectedReservation.time}
-                </p>
-                <p>
-                  <strong>Doktor:</strong>{' '}
-                  {selectedReservation.doctor.firstName}+{' '}
-                  {selectedReservation.doctor.lastName}
-                </p>
-                <p>
-                  <strong>Ambulance:</strong>
-                  {selectedReservation.clinic.name}
-                </p>
-                <p>
-                  <strong>Poznámka:</strong>
-                  {selectedReservation.note}
-                </p>
-              </div>
-            )}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={handleCloseModal}>
-              Zavřít
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Container>
-      <FooterManagement />
-    </Fragment>
-  );
+								.map((reservation) => (
+									<tr key={reservation.id}>
+										<td>{reservation.id}</td>
+										<td>{reservation.client.firstName}</td>
+										<td>{reservation.client.lastName}</td>
+										<td>
+											{getFormattedDate(new Date(reservation.date))}
+											{' v '}
+											{reservation.time}
+										</td>
+										<td>
+											{reservation.doctor.firstName}{' '}
+											{reservation.doctor.lastName}
+										</td>
+										<td>{reservation.clinic.name}</td>
+										<td>
+											<Button
+												variant='info'
+												size='lg'
+												className='mr-1 me-1'
+												onClick={() => handleShowInfoModal(reservation)}>
+												<InfoCircle />
+											</Button>
+											<EditReservation
+												Reservation={reservation as Reservation}
+												ReservationList={reservationsList}
+												SetReservationList={setReservationsList}
+											/>
+											<Button
+												variant='danger'
+												size='lg'
+												onClick={() => handleDeleteReservation(reservation)}>
+												<Trash3Fill />
+											</Button>
+										</td>
+									</tr>
+								))}
+					</tbody>
+				</Table>
+				<AddReservation managementMode={true} />
+				<Modal
+					show={showInfoModal}
+					onHide={handleCloseModal}>
+					<Modal.Header closeButton>
+						<Modal.Title>Informace o rezervaci</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						{selectedReservation && (
+							<div>
+								<p>
+									<strong>Číslo:</strong> {selectedReservation.id}
+								</p>
+								<p>
+									<strong>Jméno:</strong> {selectedReservation.client.firstName}
+								</p>
+								<p>
+									<strong>Příjmení:</strong>{' '}
+									{selectedReservation.client.lastName}
+								</p>
+								<p>
+									<strong>Telefon:</strong>
+									{selectedReservation.client.phoneNumber}
+								</p>
+								<p>
+									<strong>E-mail:</strong> {selectedReservation.client.email}
+								</p>
+								<p>
+									<strong>Datum: </strong>
+									{getFormattedDate(new Date(selectedReservation.date))}
+								</p>
+								<p>
+									<strong>Čas:</strong> {selectedReservation.time}
+								</p>
+								<p>
+									<strong>Doktor:</strong>{' '}
+									{selectedReservation.doctor.firstName}+{' '}
+									{selectedReservation.doctor.lastName}
+								</p>
+								<p>
+									<strong>Ambulance:</strong>
+									{selectedReservation.clinic.name}
+								</p>
+								<p>
+									<strong>Poznámka:</strong>
+									{selectedReservation.note}
+								</p>
+							</div>
+						)}
+					</Modal.Body>
+					<Modal.Footer>
+						<Button
+							variant='secondary'
+							onClick={handleCloseModal}>
+							Zavřít
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</Container>
+			<FooterManagement />
+		</Fragment>
+	);
 };
 
 export default MyReservations;

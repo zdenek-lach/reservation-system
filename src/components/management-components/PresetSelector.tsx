62d496ac0fe96dc7fe8877e1c556ebca0d0352c3
Sidonius <lach.zdenek@gmail.com>
2024-03-31 12:00:21 +0200

    This code seems to work, but it broke somewhere else. FIX: TBD

--- src/components/management-components/PresetSelector.tsx
+++ src/components/management-components/PresetSelector.tsx
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { useAppContext } from 'context/AppContext';
-import { useEffect, useMemo, useState } from 'react';
+import {
+	useEffect,
+	useMemo,
+	useState,
+	type Dispatch,
+	type SetStateAction,
+} from 'react';
 import {
 	Button,
 	Col,
@@ -14,15 +20,20 @@ import {
 import { authHeader } from 'security/AuthService';
 import { CenterSpinner } from 'styles/StyledComponentsLib';
 import Doctor from 'types/DoctorType';
+import type PresetType from 'types/PresetType';
 import config from '../../../config/config.json';
 import WeekGrid2, { TimeSlot } from './WeekGrid2';
 
 type PresetSelectorProps = {
 	presetName?: string;
 	setPresetName?: (value: string) => void;
-	clickedButtons: TimeSlot[];
+	clickedButtons?: TimeSlot[];
 	loggedInDoctor: Doctor | null;
 	loading: boolean;
+	isReadOnly?: boolean;
+	onPresetSelected?: (preset: PresetType) => void;
+	setInitialShifts?: Dispatch<SetStateAction<TimeSlot[]>>;
+	currentWeek?: Date;
 };
 
 const PresetSelector = ({
@@ -30,8 +41,12 @@ const PresetSelector = ({
 	setPresetName,
 	loggedInDoctor,
 	loading,
-}) => {
-	const { selectedPreset, setSelectedPreset, currentWeek } = useAppContext();
+	isReadOnly,
+	onPresetSelected,
+	setInitialShifts,
+	currentWeek,
+}: PresetSelectorProps) => {
+	const { selectedPreset, setSelectedPreset } = useAppContext();
 	const [presetList, setPresetList] = useState([]);
 	const [clickedButtons, setClickedButtons] = useState<TimeSlot[]>([]);
 	const [shouldPresetBeRenamed, setShouldPresetBeRenamed] = useState(true);
@@ -249,123 +264,185 @@ const PresetSelector = ({
 
 		return shifts;
 	}, [selectedPreset, currentWeek]);
+
+	const convertPresetToTimeBlocks = (
+		preset: PresetType,
+		currentWeek: Date
+	): TimeSlot[] => {
+		const daysOfWeek = [
+			'monday',
+			'tuesday',
+			'wednesday',
+			'thursday',
+			'friday',
+			'saturday',
+			'sunday',
+		];
+		let timeSlots: TimeSlot[] = [];
+
+		daysOfWeek.forEach((day, index) => {
+			const dayDate = new Date(
+				currentWeek.getFullYear(),
+				currentWeek.getMonth(),
+				currentWeek.getDate() + index,
+				0,
+				0,
+				0,
+				0 // Sets the time to 00:00:00.000
+			);
+
+			preset[day].forEach((time) => {
+				timeSlots.push({ day: dayDate, time });
+			});
+		});
+
+		return timeSlots;
+	};
+
+	const handlePresetSelected = (preset: PresetType) => {
+		setSelectedPreset(preset); // Set the selected preset
+
+		// Convert the selected preset into a format that can be used by the WeekGrid2 component
+		const convertedPresetsToTimeslots = convertPresetToTimeBlocks(
+			preset,
+			currentWeek
+		);
+		// Update the initial shifts to match the selected preset
+		setInitialShifts(convertedPresetsToTimeslots);
+	};
+
 	// Render the dropdown and preset options/buttons
+	const renderDropDown = () => {
+		return (
+			<Dropdown>
+				<Dropdown.Toggle
+					variant='success'
+					className='me-2 mt-3 mb-3'>
+					{selectedPreset != null
+						? `Preset ${selectedPreset.name}`
+						: !isReadOnly
+						? 'Vytvořit nový preset'
+						: 'Vyberte preset'}
+				</Dropdown.Toggle>
+				<Dropdown.Menu>
+					{!isReadOnly && (
+						<>
+							<Dropdown.Item onClick={() => setSelectedPreset(null)}>
+								Nový Preset
+							</Dropdown.Item>
+						</>
+					)}
+					{isReadOnly &&
+						presetList &&
+						presetList.map((preset) => (
+							<Dropdown.Item
+								key={preset.id}
+								onClick={() => handlePresetSelected(preset)}>
+								{`Preset ${preset.name}`}
+							</Dropdown.Item>
+						))}
+				</Dropdown.Menu>
+			</Dropdown>
+		);
+	};
+	if (isReadOnly) {
+		return renderDropDown();
+	}
 	return (
-		<Container>
-			<Row>
-				<Col sm={3}>
-					<Dropdown>
-						<Dropdown.Toggle
-							variant='success'
-							className='me-2 mt-3 mb-3'>
-							{selectedPreset != null
-								? `Preset ${selectedPreset.name}`
-								: 'Vytvořit nový preset'}
-							<Dropdown.Menu>
-								<Dropdown.Item onClick={() => setSelectedPreset(null)}>
-									Nový Preset
-								</Dropdown.Item>
-								{presetList &&
-									presetList.map((preset) => (
-										<Dropdown.Item
-											key={preset.id}
-											onClick={() => setSelectedPreset(preset)}>
-											{`Preset ${preset.name}`}
-										</Dropdown.Item>
-									))}
-							</Dropdown.Menu>
-						</Dropdown.Toggle>
-					</Dropdown>
-				</Col>
-				<Col sm={3}>
-					{selectedPreset != null && (
-						<Button
-							variant='danger'
-							onClick={deleteSelectedPreset}>
-							Odstranit vybraný preset
-						</Button>
-					)}
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={4}>
-					{selectedPreset == null && (
-						<InputGroup>
-							{shouldPresetBeRenamed && (
-								<Form.Control
-									placeholder='Zvolte jméno nového presetu'
-									aria-describedby='preset-name'
-									value={presetName || ''}
-									onChange={(e) => setPresetName(e.target.value)}
+		<>
+			<Container>
+				<Row>
+					<Col sm={3}>{renderDropDown()}</Col>
+					<Col sm={3}>
+						{selectedPreset != null && (
+							<Button
+								variant='danger'
+								onClick={deleteSelectedPreset}>
+								Odstranit vybraný preset
+							</Button>
+						)}
+					</Col>
+				</Row>
+				<Row>
+					<Col sm={4}>
+						{selectedPreset == null && (
+							<InputGroup>
+								{shouldPresetBeRenamed && (
+									<Form.Control
+										placeholder='Zvolte jméno nového presetu'
+										aria-describedby='preset-name'
+										value={presetName || ''}
+										onChange={(e) => setPresetName(e.target.value)}
+									/>
+								)}
+							</InputGroup>
+						)}
+						{selectedPreset != null && (
+							<Form>
+								<FormCheck
+									type='switch'
+									label='Ponechat jméno presetu'
+									checked={shouldPresetBeRenamed}
+									onChange={() =>
+										setShouldPresetBeRenamed((prevValue) => !prevValue)
+									}
+									className='mb-3'
 								/>
-							)}
-						</InputGroup>
-					)}
-					{selectedPreset != null && (
-						<Form>
-							<FormCheck
-								type='switch'
-								label='Ponechat jméno presetu'
-								checked={shouldPresetBeRenamed}
-								onChange={() =>
-									setShouldPresetBeRenamed((prevValue) => !prevValue)
-								}
-								className='mb-3'
-							/>
-						</Form>
-					)}
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={12}>
-					{selectedPreset != null && !shouldPresetBeRenamed && (
-						<InputGroup className='mb-3'>
-							<Form.Control
-								placeholder='Jméno presetu'
-								aria-label='Jméno presetu'
-								aria-describedby='preset-name'
-								value={selectedPreset.name.toString()}
-								onChange={(e) => {
-									setPresetName(e.target.value);
-									setSelectedPreset({
-										...selectedPreset,
-										name: e.target.value,
-									});
-								}}
-							/>
-						</InputGroup>
-					)}
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={12}>
-					<WeekGrid2
-						startOfWeek={currentWeek}
-						initialShifts={initialShifts}
-						setClickedButtons={setClickedButtons}
-						isPresetMode={true}
-					/>
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={4}>
-					{selectedPreset == null && (
-						<Button
-							variant='primary'
-							onClick={submitNewPreset}>
-							Přidat nový preset
-						</Button>
-					)}
-					{selectedPreset != null && (
-						<Button
-							variant='warning'
-							onClick={editSelectedPreset}>
-							Uložit provedené změny
-						</Button>
-					)}
-				</Col>
-			</Row>
-		</Container>
+							</Form>
+						)}
+					</Col>
+				</Row>
+				<Row>
+					<Col sm={12}>
+						{selectedPreset != null && !shouldPresetBeRenamed && (
+							<InputGroup className='mb-3'>
+								<Form.Control
+									placeholder='Jméno presetu'
+									aria-label='Jméno presetu'
+									aria-describedby='preset-name'
+									value={selectedPreset.name.toString()}
+									onChange={(e) => {
+										setPresetName(e.target.value);
+										setSelectedPreset({
+											...selectedPreset,
+											name: e.target.value,
+										});
+									}}
+								/>
+							</InputGroup>
+						)}
+					</Col>
+				</Row>
+				<Row>
+					<Col sm={12}>
+						<WeekGrid2
+							startOfWeek={currentWeek}
+							initialShifts={initialShifts}
+							setClickedButtons={setClickedButtons}
+							isPresetMode={true}
+						/>
+					</Col>
+				</Row>
+				<Row>
+					<Col sm={4}>
+						{selectedPreset == null && (
+							<Button
+								variant='primary'
+								onClick={submitNewPreset}>
+								Přidat nový preset
+							</Button>
+						)}
+						{selectedPreset != null && (
+							<Button
+								variant='warning'
+								onClick={editSelectedPreset}>
+								Uložit provedené změny
+							</Button>
+						)}
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 };
 

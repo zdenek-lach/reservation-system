1125f899bea219631ef66c3ae92b8919968cf7ba
Sidonius <lach.zdenek@gmail.com>
2024-04-13 19:01:42 +0200

    .

--- src/components/MessageToast.tsx
+++ src/components/MessageToast.tsx
@@ -1,44 +1,32 @@
 import { useAppContext } from 'context/AppContext';
 import React from 'react';
-import { Toast } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 
 interface MessageToastProps {
-  message: string;
+	message: string;
 }
 
 const MessageToast: React.FC<MessageToastProps> = ({ message }) => {
-  const { showMessageToast, setShowMessageToast } = useAppContext();
+	const { showMessageToast, setShowMessageToast } = useAppContext();
 
-  const handleClose = () => setShowMessageToast(false);
+	const handleClose = () => setShowMessageToast(false);
 
-  return (
-    <Toast
-      onClose={handleClose}
-      show={showMessageToast}
-      delay={3000}
-      autohide
-      animation
-      className="shadow-lg p-3 mb-5"
-      style={{
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 9999,
-      }}
-    >
-      <Toast.Body
-        className=""
-        style={{
-          fontSize: '1.5rem',
-          backgroundColor: 'green',
-          color: '#fff',
-        }}
-      >
-        {message}
-      </Toast.Body>
-    </Toast>
-  );
+	return (
+		<Alert
+			variant='success'
+			onClose={handleClose}
+			style={{
+				position: 'fixed',
+				top: '50%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)',
+				zIndex: 9999,
+				fontSize: '1.5rem',
+			}}
+			show={showMessageToast}>
+			{message}
+		</Alert>
+	);
 };
 
 export default MessageToast;

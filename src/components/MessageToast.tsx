a2b6afa17639b54eac4f293cadd35804172689a8
Sidonius <lach.zdenek@gmail.com>
2024-04-13 19:18:29 +0200

    Adding config for alert, new alert, APITester added auth and fixed Jackie's forgotten fragment

--- src/components/MessageToast.tsx
+++ src/components/MessageToast.tsx
@@ -1,7 +1,7 @@
 import { useAppContext } from 'context/AppContext';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Alert } from 'react-bootstrap';
-
+import config from '../../config/config.json'
 interface MessageToastProps {
 	message: string;
 }
@@ -9,19 +9,29 @@ interface MessageToastProps {
 const MessageToast: React.FC<MessageToastProps> = ({ message }) => {
 	const { showMessageToast, setShowMessageToast } = useAppContext();
 
-	const handleClose = () => setShowMessageToast(false);
+	useEffect(() => {
+		if (showMessageToast) {
+			const timer = setTimeout(() => {
+				setShowMessageToast(false);
+			}, config.general.toastTimeoutInSeconds*1000); // Auto-hide after set interval from config
+
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	}, [showMessageToast, setShowMessageToast]);
 
 	return (
 		<Alert
 			variant='success'
-			onClose={handleClose}
+			onClose={() => setShowMessageToast(false)}
 			style={{
 				position: 'fixed',
 				top: '50%',
 				left: '50%',
 				transform: 'translate(-50%, -50%)',
 				zIndex: 9999,
-				fontSize: '1.5rem',
+				fontSize: '2rem',
 			}}
 			show={showMessageToast}>
 			{message}

b77a336e7285a63be36c4ae9a26ab8a7522930ae
Sidonius <lach.zdenek@gmail.com>
2024-04-13 21:43:54 +0200

    Added Shift-Management page, refactored ShiftType (WIP)

--- src/components/management-components/WeekGrid2.tsx
+++ src/components/management-components/WeekGrid2.tsx
@@ -1,155 +1,156 @@
 import { eachHourOfInterval, format } from 'date-fns';
 import { useEffect, useState } from 'react';
 import { Badge, Button, Table } from 'react-bootstrap';
+import { Shift as TimeSlot } from '../../types/ShiftType';
 
 interface WeekGrid2Props {
-  startOfWeek: Date;
-  setClickedButtons: React.Dispatch<React.SetStateAction<TimeSlot[]>>;
-  initialShifts?: TimeSlot[];
-  readOnly?: boolean;
-  isPresetMode?: boolean;
+	startOfWeek: Date;
+	setClickedButtons: React.Dispatch<React.SetStateAction<TimeSlot[]>>;
+	initialShifts?: TimeSlot[];
+	readOnly?: boolean;
+	isPresetMode?: boolean;
 }
 const startHour = 7;
 const endHour = 19;
 const timeSlots = eachHourOfInterval({
-  start: new Date(0, 0, 0, startHour),
-  end: new Date(0, 0, 0, endHour),
+	start: new Date(0, 0, 0, startHour),
+	end: new Date(0, 0, 0, endHour),
 }).map((time) => format(time, 'HH:mm'));
 
-export interface TimeSlot {
-  day: Date;
-  time: string;
-}
-
 const WeekGrid2: React.FC<WeekGrid2Props> = ({
-  startOfWeek,
-  setClickedButtons,
-  initialShifts,
-  readOnly = false,
-  isPresetMode = false,
+	startOfWeek,
+	setClickedButtons,
+	initialShifts,
+	readOnly = false,
+	isPresetMode = false,
 }) => {
-  const days = ['Po', 'Út', 'St', 'Čt', 'Pá'];
+	const days = ['Po', 'Út', 'St', 'Čt', 'Pá'];
 
-  const getInitialShifts = () => {
-    let initialButtonStates: string[] = [];
-    initialShifts?.forEach((slot) => {
-      const buttonKey = `${slot.day.getFullYear()}-${slot.day.getMonth()}-${slot.day.getDate()}-${
-        slot.time
-      }`;
-      initialButtonStates.push(buttonKey);
-    });
-    return initialButtonStates;
-  };
+	const getInitialShifts = () => {
+		let initialButtonStates: string[] = [];
+		initialShifts?.forEach((slot) => {
+			const buttonKey = `${slot.day.getFullYear()}-${slot.day.getMonth()}-${slot.day.getDate()}-${
+				slot.time
+			}`;
+			initialButtonStates.push(buttonKey);
+		});
+		return initialButtonStates;
+	};
 
-  const [buttonStates, setButtonStates] = useState<string[]>([]);
-  const [selectedTimes, setSelectedTimes] = useState([]);
+	const [buttonStates, setButtonStates] = useState<string[]>([]);
+	const [selectedTimes, setSelectedTimes] = useState([]);
 
-  const getSelectedTimes = () => {
-    let times: TimeSlot[] = [];
-    initialShifts?.forEach((slot) => {
-      times.push({ day: slot.day, time: slot.time });
-    });
-    return times;
-  };
+	const getSelectedTimes = () => {
+		let times: TimeSlot[] = [];
+		initialShifts?.forEach((slot) => {
+			times.push({ day: slot.day, time: slot.time });
+		});
+		return times;
+	};
 
-  useEffect(() => {
-    if (initialShifts) {
-      const shifts = getInitialShifts();
-      const times = getSelectedTimes();
-      setButtonStates(shifts);
-      setSelectedTimes(times);
-    }
-  }, [initialShifts]);
+	useEffect(() => {
+		if (initialShifts) {
+			const shifts = getInitialShifts();
+			const times = getSelectedTimes();
+			setButtonStates(shifts);
+			setSelectedTimes(times);
+		}
+	}, [initialShifts]);
 
-  const getIsSelected = (buttonStates: string[], buttonKey: string) => {
-    for (const state of buttonStates) {
-      if (state == buttonKey) {
-        return true;
-      }
-    }
-    return false;
-  };
+	const getIsSelected = (buttonStates: string[], buttonKey: string) => {
+		for (const state of buttonStates) {
+			if (state == buttonKey) {
+				return true;
+			}
+		}
+		return false;
+	};
 
-  const addOrRemoveSelectedTime = (dayDate: Date, time: string) => {
-    const buttonKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
-    const isSelected = getIsSelected(buttonStates, buttonKey);
+	const addOrRemoveSelectedTime = (dayDate: Date, time: string) => {
+		const buttonKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
+		const isSelected = getIsSelected(buttonStates, buttonKey);
 
-    let newButtonStates = [...buttonStates];
-    let newSelectedTimes = [...selectedTimes];
+		let newButtonStates = [...buttonStates];
+		let newSelectedTimes = [...selectedTimes];
 
-    if (isSelected) {
-      newButtonStates = newButtonStates.filter((state) => {
-        return state != buttonKey;
-      });
-      newSelectedTimes = newSelectedTimes.filter((slot) => {
-        const date1 = `${slot.day.getFullYear()}-${slot.day.getMonth()}-${slot.day.getDate()}-${
-          slot.time
-        }`;
-        const date2 = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
-        return date1 != date2;
-      });
-    } else {
-      newButtonStates.push(buttonKey);
-      newSelectedTimes.push({ day: dayDate, time });
-    }
+		if (isSelected) {
+			newButtonStates = newButtonStates.filter((state) => {
+				return state != buttonKey;
+			});
+			newSelectedTimes = newSelectedTimes.filter((slot) => {
+				const date1 = `${slot.day.getFullYear()}-${slot.day.getMonth()}-${slot.day.getDate()}-${
+					slot.time
+				}`;
+				const date2 = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
+				return date1 != date2;
+			});
+		} else {
+			newButtonStates.push(buttonKey);
+			newSelectedTimes.push({ day: dayDate, time });
+		}
 
-    setButtonStates(newButtonStates);
-    setSelectedTimes(newSelectedTimes);
-  };
+		setButtonStates(newButtonStates);
+		setSelectedTimes(newSelectedTimes);
+	};
 
-  useEffect(() => {
-    setClickedButtons(selectedTimes);
-  }, [selectedTimes, setClickedButtons]);
-  return (
-    <Table striped size='md'>
-      <tbody>
-        {days.map((day, index) => {
-          const dayDate = new Date(
-            startOfWeek.getFullYear(),
-            startOfWeek.getMonth(),
-            startOfWeek.getDate() + index,
-            0,
-            0,
-            0,
-            0 // Sets the time to 00:00:00.000
-          );
-          return (
-            <tr key={day} className='bg-light text-dark'>
-              <td>
-                {day}
-                {!isPresetMode && (
-                  <Badge className=''>
-                    {dayDate.toLocaleDateString('cs-CZ')}
-                  </Badge>
-                )}
-              </td>
-              {timeSlots.map((time) => {
-                const buttonKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
-                const isSelected = getIsSelected(buttonStates, buttonKey);
+	useEffect(() => {
+		setClickedButtons(selectedTimes);
+	}, [selectedTimes, setClickedButtons]);
+	return (
+		<Table
+			striped
+			size='md'>
+			<tbody>
+				{days.map((day, index) => {
+					const dayDate = new Date(
+						startOfWeek.getFullYear(),
+						startOfWeek.getMonth(),
+						startOfWeek.getDate() + index,
+						0,
+						0,
+						0,
+						0 // Sets the time to 00:00:00.000
+					);
+					return (
+						<tr
+							key={day}
+							className='bg-light text-dark'>
+							<td>
+								{day}
+								{!isPresetMode && (
+									<Badge className=''>
+										{dayDate.toLocaleDateString('cs-CZ')}
+									</Badge>
+								)}
+							</td>
+							{timeSlots.map((time) => {
+								const buttonKey = `${dayDate.getFullYear()}-${dayDate.getMonth()}-${dayDate.getDate()}-${time}`;
+								const isSelected = getIsSelected(buttonStates, buttonKey);
 
-                return (
-                  <td key={buttonKey} className='p-2'>
-                    <Button
-                      style={{
-                        borderRadius: '20px',
-                      }}
-                      variant={isSelected ? 'primary' : 'secondary'}
-                      onClick={() =>
-                        !readOnly && addOrRemoveSelectedTime(dayDate, time)
-                      }
-                    >
-                      {time}
-                      {!isPresetMode && ' - ' + dayDate.toLocaleDateString()}
-                    </Button>
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
-  );
+								return (
+									<td
+										key={buttonKey}
+										className='p-2'>
+										<Button
+											style={{
+												borderRadius: '20px',
+											}}
+											variant={isSelected ? 'primary' : 'secondary'}
+											onClick={() =>
+												!readOnly && addOrRemoveSelectedTime(dayDate, time)
+											}>
+											{time}
+											{!isPresetMode && ' - ' + dayDate.toLocaleDateString()}
+										</Button>
+									</td>
+								);
+							})}
+						</tr>
+					);
+				})}
+			</tbody>
+		</Table>
+	);
 };
 
 export default WeekGrid2;

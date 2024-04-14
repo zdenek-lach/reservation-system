8e52d02b0d36ab9b7a63cd8b97717d93854aa658
Sidonius <lach.zdenek@gmail.com>
2024-04-14 15:10:12 +0200

    Shift Management improvements + some refactoring.

--- src/styles/StyledComponentsLib.ts
+++ src/styles/StyledComponentsLib.ts
@@ -1,5 +1,5 @@
 import { Spinner } from 'react-bootstrap';
-import styled from 'styled-components';
+import styled, { type CSSProperties } from 'styled-components';
 
 export const CenterSpinner = styled(Spinner)`
   display: block;
@@ -12,3 +12,26 @@ export const CenterSpinner = styled(Spinner)`
     -50%
   ); 
 `;
+
+export const cardStyle: CSSProperties = {
+	borderRadius: '15px',
+	fontSize: '2em',
+	backgroundColor: '#dc3545',
+	color: '#ffffff',
+	padding: '20px',
+	textAlign: 'center',
+	marginBottom: '20px',
+	alignItems: 'center',
+	transition: 'background-color 0.3s ease-in-out',
+};
+
+export const globalSettingsStyle: CSSProperties = {
+	...cardStyle,
+	backgroundColor: '#808080',
+};
+
+export const iconStyle: CSSProperties = {
+	color: '#ffffff',
+	fontSize: '2em',
+	transition: 'color 0.3s ease-in-out',
+};

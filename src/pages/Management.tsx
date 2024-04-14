541252b3fde7c853861360d3b2531b1c75967ec5
Sidonius <lach.zdenek@gmail.com>
2024-04-14 15:10:12 +0200

    Shift Management improvements + some refactoring.

--- src/pages/Management.tsx
+++ src/pages/Management.tsx
@@ -3,144 +3,132 @@ import FooterManagement from 'components/management-components/FooterManagement'
 import { Fragment, useEffect, useState } from 'react';
 import { Card, Col, Container, Row, Spinner } from 'react-bootstrap';
 import {
-  CalendarPlusFill,
-  ClipboardCheckFill,
-  GearWide,
-  NodePlusFill,
-  PersonFillGear,
-  PersonLinesFill,
+	Calendar4Week,
+	CalendarPlusFill,
+	ClipboardCheckFill,
+	GearWide,
+	NodePlusFill,
+	PersonFillGear,
+	PersonLinesFill,
 } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router';
-import { CSSProperties } from 'styled-components';
 import Doctor from 'types/DoctorType';
 import { fetchLoggedDoctor } from '../security/AuthService';
-
-const cardStyle: CSSProperties = {
-  borderRadius: '15px',
-  fontSize: '2em',
-  backgroundColor: '#dc3545',
-  color: '#ffffff',
-  padding: '20px',
-  textAlign: 'center',
-  marginBottom: '20px',
-  alignItems: 'center',
-  transition: 'background-color 0.3s ease-in-out',
-};
-
-const globalSettingsStyle: CSSProperties = {
-  ...cardStyle,
-  backgroundColor: '#808080',
-};
-
-const iconStyle: CSSProperties = {
-  color: '#ffffff',
-  fontSize: '2em',
-  transition: 'color 0.3s ease-in-out',
-};
+import {
+	cardStyle,
+	globalSettingsStyle,
+	iconStyle,
+} from '../styles/StyledComponentsLib';
 
 const Management = () => {
-  const navigate = useNavigate();
-  const [doctor, setDoctor] = useState<Doctor | null>(null);
-  const [loadingDoctor, setIsLoadingDoctor] = useState(true);
+	const navigate = useNavigate();
+	const [doctor, setDoctor] = useState<Doctor | null>(null);
+	const [loadingDoctor, setIsLoadingDoctor] = useState(true);
 
-  const handleCardHover = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.currentTarget.style.backgroundColor = '#b02a37';
-  };
+	const handleCardHover = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.currentTarget.style.backgroundColor = '#b02a37';
+	};
 
-  const handleCardLeave = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.currentTarget.style.backgroundColor = '#dc3545';
-  };
+	const handleCardLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.currentTarget.style.backgroundColor = '#dc3545';
+	};
 
-  useEffect(() => {
-    fetchLoggedDoctor().then((user) => {
-      setDoctor(user);
-      setIsLoadingDoctor(false);
-    });
-  }, []);
+	useEffect(() => {
+		fetchLoggedDoctor().then((user) => {
+			setDoctor(user);
+			setIsLoadingDoctor(false);
+		});
+	}, []);
 
-  const renderCard = (
-    title: string,
-    icon: JSX.Element,
-    onClick: () => void,
-    isLoading?: boolean
-  ) => (
-    <Col md={4} key={title}>
-      <Card
-        style={cardStyle}
-        onMouseEnter={handleCardHover}
-        onMouseLeave={handleCardLeave}
-        onClick={onClick}
-      >
-        {!isLoading && (
-          <>
-            {icon}
-            {title}
-          </>
-        )}
-        {isLoading && <Spinner />}
-      </Card>
-    </Col>
-  );
+	const renderCardCol = (
+		title: string,
+		icon: JSX.Element,
+		onClick: () => void,
+		isLoading?: boolean
+	) => (
+		<Col
+			md={4}
+			key={title}>
+			<Card
+				style={cardStyle}
+				onMouseEnter={handleCardHover}
+				onMouseLeave={handleCardLeave}
+				onClick={onClick}>
+				{!isLoading && (
+					<>
+						{icon}
+						{title}
+					</>
+				)}
+				{isLoading && <Spinner />}
+			</Card>
+		</Col>
+	);
 
-  return (
-    <Fragment>
-      <Container>
-        <Row className='mb-4 mt-4'>
-          {renderCard(
-            'Můj profil',
-            <PersonLinesFill style={iconStyle} />,
-            () => navigate('my-profile'),
-            loadingDoctor
-          )}
-          {renderCard(
-            'Moje směny',
-            <CalendarPlusFill style={iconStyle} />,
-            () => navigate('my-shifts'),
-            loadingDoctor
-          )}
-          {renderCard(
-            'Moje rezervace',
-            <NodePlusFill style={iconStyle} />,
-            () => navigate('my-reservations'),
-            loadingDoctor
-          )}
-        </Row>
-        <Row className='mb-4'>
-          {renderCard(
-            'Správa rezervací',
-            <ClipboardCheckFill style={iconStyle} />,
-            () => navigate('reservation-management')
-          )}
-          {renderCard(
-            'Správa zaměstnanců',
-            <PersonFillGear style={iconStyle} />,
-            () => navigate('employee-management')
-          )}
-          <Col
-            md={4}
-            key={'Globální nastavení'}
-            onClick={() => {
-              navigate('global-settings');
-            }}
-          >
-            <Card
-              style={globalSettingsStyle}
-              onMouseEnter={(e) => {
-                e.currentTarget.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
-              }}
-              onMouseLeave={(e) => {
-                e.currentTarget.style.backgroundColor = '#808080';
-              }}
-            >
-              <GearWide style={iconStyle} />
-              Globální nastavení
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      <FooterManagement />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Container>
+				<Row className='mb-4 mt-4'>
+					{renderCardCol(
+						'Můj profil',
+						<PersonLinesFill style={iconStyle} />,
+						() => navigate('my-profile'),
+						loadingDoctor
+					)}
+					{renderCardCol(
+						'Moje směny',
+						<CalendarPlusFill style={iconStyle} />,
+						() => navigate('my-shifts'),
+						loadingDoctor
+					)}
+					{renderCardCol(
+						'Moje rezervace',
+						<NodePlusFill style={iconStyle} />,
+						() => navigate('my-reservations'),
+						loadingDoctor
+					)}
+				</Row>
+				<Row className='mb-4'>
+					{renderCardCol(
+						'Správa rezervací',
+						<ClipboardCheckFill style={iconStyle} />,
+						() => navigate('reservation-management')
+					)}
+					{renderCardCol(
+						'Správa zaměstnanců',
+						<PersonFillGear style={iconStyle} />,
+						() => navigate('employee-management')
+					)}
+					{renderCardCol(
+						'Správa směn',
+						<Calendar4Week style={iconStyle} />,
+						() => navigate('shift-management')
+					)}
+				</Row>
+				<Row>
+					<Col
+						md={4}
+						key={'Globální nastavení'}
+						onClick={() => {
+							navigate('global-settings');
+						}}>
+						<Card
+							style={globalSettingsStyle}
+							onMouseEnter={(e) => {
+								e.currentTarget.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+							}}
+							onMouseLeave={(e) => {
+								e.currentTarget.style.backgroundColor = '#808080';
+							}}>
+							<GearWide style={iconStyle} />
+							Globální nastavení
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+			<FooterManagement />
+		</Fragment>
+	);
 };
 
 export default Management;

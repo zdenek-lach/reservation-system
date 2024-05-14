1f573160ad407176c9b1f4c6c6d046eb69296fc0
Sidonius <lach.zdenek@gmail.com>
2024-05-14 19:51:40 +0200

    Delete function implementation.

--- src/pages/ShiftManagement.tsx
+++ src/pages/ShiftManagement.tsx
@@ -3,7 +3,7 @@ import { getFormattedDate } from 'components/WeekPicker';
 import ClinicSelector from 'components/management-components/ClinicSelector';
 import DoctorSelector from 'components/management-components/DoctorSelector';
 import { useClinics } from 'hooks/useClinics';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap';
 import {
 	ArrowCounterclockwise,
@@ -16,7 +16,6 @@ import Clinic from 'types/ClinicType';
 import type { ShiftApi } from 'types/ShiftType';
 import config from '../../config/config.json';
 import Doctor from './../types/DoctorType';
-import React from 'react';
 
 const ShiftManagement = () => {
 	const [loadingShifts, setLoadingShifts] = useState(true);
@@ -111,6 +110,48 @@ const ShiftManagement = () => {
 	const handleDateChange = (date: any) => {
 		setSelectedDate(date);
 	};
+	const handleDeleteShift = (date, time) => {
+		// Find the shift in the original shiftList with matching date and time
+		const shiftToDelete = shiftList.find((shift) =>
+			shift.shifts.some(
+				(shiftDetail) =>
+					getFormattedDate(new Date(shiftDetail.date)) === getFormattedDate(new Date(date)) &&
+					shiftDetail.time === time
+			)
+		);
+
+		// If the shift is found, extract its ID and delete it
+		if (shiftToDelete) {
+			const shiftIdToDelete = shiftToDelete.id;
+			const deleteUrl = `${config.api.shiftApi.delete}/${shiftIdToDelete}`;
+
+			console.warn(`deleting shift id: ${shiftIdToDelete}`);
+			axios
+				.delete(deleteUrl, {
+					headers: {
+						...authHeader(),
+					},
+				})
+				.then((response) => {
+					console.log(`Successfully deleted shift id: ${shiftIdToDelete}`);
+					console.log(response.status);
+
+					// Remove the deleted shift from the shiftList
+					if (shiftList) {
+						const updatedShifts = shiftList.filter(
+							(shift) => shift.id !== shiftIdToDelete
+						);
+						setShiftList(updatedShifts);
+					}
+				})
+				.catch((error) => {
+					console.error(`Error deleting shift id: ${shiftIdToDelete}`, error);
+				});
+		} else {
+			console.error(`Shift not found for date: ${date} and time: ${time}`);
+		}
+	};
+
 	return (
 		<>
 			<Container
@@ -246,7 +287,13 @@ const ShiftManagement = () => {
 														</Button>
 														<Button
 															variant='danger'
-															size='lg'>
+															size='lg'
+															onClick={() =>
+																handleDeleteShift(
+																	shiftDetail.date,
+																	shiftDetail.time
+																)
+															}>
 															<Trash3Fill />
 														</Button>
 													</td>

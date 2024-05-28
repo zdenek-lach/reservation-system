a7d2398260366639f9d5a2a44bbeb5286eefbadb
markooLohn <lohn@edhouse.cz>
2024-05-28 20:05:00 +0200

    Big Calendar Test - Looks promising

--- src/types/ShiftType.ts
+++ src/types/ShiftType.ts
@@ -2,16 +2,26 @@ import type Clinic from './ClinicType';
 import type Doctor from './DoctorType';
 
 export type Shift = {
-	id?: number;
-	day?: Date;
-	date?: Date;
-	time: string;
+  id?: number;
+  day?: Date;
+  date?: Date;
+  time: string;
 };
 
 export type ShiftApi = {
-	id: number;
-	doctor: Doctor;
-	clinic: Clinic;
-	shifts: Shift[];
+  id: number;
+  doctor: Doctor;
+  clinic: Clinic;
+  shifts: Shift[];
 };
+
+export type ShiftApiNormal = {
+  id: number;
+  doctor: Doctor;
+  clinic: Clinic;
+  date: string;
+  timeFrom: string;
+  timeTo: string;
+};
+
 export default Shift;
